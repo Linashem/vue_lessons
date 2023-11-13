a524966110068542e824ca9f9ab620721e88585d
Linashem <lina.shemeneva@yandex.ru>
2023-11-13 03:36:13 +0300

    аппи, реактивные переменные, методы

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,13 +2,31 @@
 import PageHeader from '@/components/PageHeader.vue';
 import PageContent from '@/components/PageContent.vue';
 import PageFooter from './components/PageFooter.vue';
+import { ref } from 'vue';
+interface IUser {
+  name: string;
+  age: number;
+}
+
+//реактивные переменные
+const user = ref<IUser>({
+  name: 'Lina',
+  age: 26
+});
+
+//методы
+const ageIncrem = () => {
+  user.value.age++;
+};
 </script>
 
 <template>
   <div class="wrapper">
     <PageHeader nameComponent="Header props" />
     <PageContent>
-      <p>Hello from slot</p>
+      <p>{{ user.name + ' - users name' }}</p>
+      <p @click="ageIncrem">{{ user.age + ' -users age' }}</p>
+
       <template #bottomSlot>
         <p>Bottom Slot</p>
       </template>
